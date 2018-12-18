a9ec4c467cf89ee5731bb5a61f7cd0b55d71689f
zhu guanchen <zhuguanchen@wcw-stud-145-109-19-79.wireless.uva.nl>
2018-12-18 17:47:37 +0100

    modified real data script

--- main.R
+++ main.R
@@ -2,66 +2,73 @@
 ######
 #simulation data
 ######
-#setwd('~/Desktop/Internship1/')
-setwd('~/../Desktop/Internship1/')
+setwd('~/Desktop/Internship1/')
+#setwd('~/../Desktop/Internship1/')
 library(tidyverse)
 #source('simulation_util.R')
 source('MaSigPro_util.R')
 source('ASCA-genes.1.2.1/sourceASCA.R')
 
 
+
 ######
 #generate data
 ######
-generate_data <- function(){
+generate_data.nocorrelation <- function(){
+  #6 time points;2 treatments;4 replicates
+  #no correlation between metabolites
   tc.GENE <- function(n, r,
-                      var11 = 0.2, var12 = 0.2, var13 = 0.2,var14 = 0.2,var15 = 0.2,
-                      var21 = 0.2, var22 = 0.2, var23 = 0.2,var24 = 0.2,var25 = 0.2,
-                      var31 = 0.2, var32 = 0.2, var33 = 0.2,var34 = 0.2,var35 = 0.2,
-                      var41 = 0.2, var42 = 0.2, var43 = 0.2,var44 = 0.2,var45 = 0.2,
-                      #var51 = 0.01, var52 = 0.01, var53 = 0.01,var54 = 0.01,var55 = 0.01,
-                      a1 = 0, a2 = 0, a3 = 0, a4 = 0,
-                      b1 = 0, b2 = 0, b3 = 0, b4 = 0,
-                      c1 = 0, c2 = 0, c3 = 0, c4 = 0,
-                      d1 = 0, d2 = 0, d3 = 0, d4 = 0,
-                      e1 = 0, e2 = 0, e3 = 0, e4 = 0)
+                      var11 = 0.2, var21 = 0.2, var31 = 0.2,var41 = 0.2,var51 = 0.2,
+                      var12 = 0.2, var22 = 0.2, var32 = 0.2,var42 = 0.2,var52 = 0.2,
+                      var13 = 0.2, var23 = 0.2, var33 = 0.2,var43 = 0.2,var53 = 0.2,
+                      var14 = 0.2, var24 = 0.2, var34 = 0.2,var44 = 0.2,var54 = 0.2,
+                      var15 = 0.2, var25 = 0.2, var35 = 0.2,var45 = 0.2,var55 = 0.2,
+                      var16 = 0.2, var26 = 0.2, var36 = 0.2,var46 = 0.2,var56 = 0.2,
+                      a1 = 0,      a2 = 0,      a3 = 0,     a4 = 0,
+                      b1 = 0,      b2 = 0,      b3 = 0,     b4 = 0,
+                      c1 = 0,      c2 = 0,      c3 = 0,     c4 = 0,
+                      d1 = 0,      d2 = 0,      d3 = 0,     d4 = 0,
+                      e1 = 0,      e2 = 0,      e3 = 0,     e4 = 0,
+                      f1 = 0,      f2 = 0,      f3 = 0,     f4 = 0)
   {
     
     tc.dat <- NULL
     for (i in 1:n) {
-      Ctl <- c(rnorm(r, a1, var11), rnorm(r, b1, var12), rnorm(r, c1, var13),rnorm(r, d1, var14),rnorm(r, e1, var15))  # Ctl group
-      Tr1 <- c(rnorm(r, a2, var21), rnorm(r, b2, var22), rnorm(r, c2, var23),rnorm(r, d2, var24),rnorm(r, e2, var25))  # Tr1 group
-      Tr2 <- c(rnorm(r, a3, var31), rnorm(r, b3, var32), rnorm(r, c3, var33),rnorm(r, d3, var34),rnorm(r, e3, var35))  # Tr2 group
-      Tr3 <- c(rnorm(r, a4, var41), rnorm(r, b4, var42), rnorm(r, c4, var43),rnorm(r, d4, var44),rnorm(r, e4, var45))  # Tr3 group
-      gene <- c(Ctl, Tr1, Tr2,Tr3)
+      #Ctl <- c(rnorm(r, a1, var11), rnorm(r, b1, var12), rnorm(r, c1, var13),rnorm(r, d1, var14),rnorm(r, e1, var15))  # Ctl group
+      #Tr1 <- c(rnorm(r, a2, var21), rnorm(r, b2, var22), rnorm(r, c2, var23),rnorm(r, d2, var24),rnorm(r, e2, var25),rnorm(r,f2,var26))  # Tr1 group
+      Tr2 <- c(rnorm(r, a3, var31), rnorm(r, b3, var32), rnorm(r, c3, var33),rnorm(r, d3, var34),rnorm(r, e3, var35),rnorm(r,f3,var36))  # Tr2 group
+      Tr3 <- c(rnorm(r, a4, var41), rnorm(r, b4, var42), rnorm(r, c4, var43),rnorm(r, d4, var44),rnorm(r, e4, var45),rnorm(r, f4,var46))  # Tr3 group
+      gene <- c(Tr2, Tr3)
       tc.dat <- rbind(tc.dat, gene)
     }
     tc.dat
   }
-  onediff <- tc.GENE(10,4,
-                     #a1=y1[1],b1=y1[2],c1=y1[3],d1=y1[4],e1=y1[5],
-                     a2=y1.t[1],b2=y1.t[2],c2=y1.t[3],d2=y1.t[4],e2=y1.t[5],
-                     a3=2,b3=2,c3=2,d3=2,e3=2)
-  #a4=y2[1],b4=y2[2],c4=y2[3],d4=y2[4],e4=y2[5]
-  twodiff.2way <- tc.GENE(10,4,
-                          #a1=y1[1],b1=y1[2],c1=y1[3],d1=y1[4],e1=y1[5],
-                          a2=y1.t[1],b2=y1.t[2],c2=y1.t[3],d2=y1.t[4],e2=y1.t[5],
-                          #a3=y2[1],b3=y2[2],c3=y2[3],d3=y2[4],e3=y2[5],
-                          a4=y2[1],b4=y2[2],c4=y2[3],d4=y2[4],e4=y2[5])
-  difftime <- tc.GENE(10,4,
-                      #a1=y1[1],b1=y1[2],c1=y1[3],d1=y1[4],e1=y1[5],
-                      a2=0.2+y2[1],b2=0.2+y2[2],c2=0.2+y2[3],d2=0.2+y2[4],e2=0.2+y2[5],
-                      a3=0.3+y2[1],b3=0.3+y2[2],c3=0.3+y2[3],d3=0.3+y2[4],e3=0.3+y2[5],
-                      a4=0.4+y2[1],b4=0.4+y2[2],c4=0.4+y2[3],d4=0.4+y2[4],e4=0.4+y2[5],
-                      var41 = 0.2, var42 = 0.2, var43 = 0.2,var44 = 0.2,var45 = 0.2)
-  small <- tc.GENE(1,4,a1=y1[1],b1=y1[2],c1=y1[3],d1=y1[4],e1=y1[5],
-                   a2=y1[1],b2=y1[2],c2=y1[3],d2=y1[4],e2=y1[5],
-                   a3=y2[1],b3=y2[2],c3=y2[3],d3=y2[4],e3=y2[5],
-                   a4=y2[1],b4=y2[2],c4=y2[3],d4=y2[4],e4=y2[5])
+  one <- tc.GENE(10,4,
+                 #a1=y1[1],b1=y1[2],c1=y1[3],d1=y1[4],e1=y1[5],
+                 #a2=y1.t[1],b2=y1.t[2],c2=y1.t[3],d2=y1.t[4],e2=y1.t[5],
+                 a3=y1[1],b3=y1[2],c3=y1[3],d3=y1[4],e3=y1[5],f3=y1[6],
+                 a4=y12[1],b4=y12[2],c4=y12[3],d4=y12[4],e4=y12[5],f4=y12[6])
+  two <- tc.GENE(10,4,
+                 #a1=y1[1],b1=y1[2],c1=y1[3],d1=y1[4],e1=y1[5],
+                 #a2=y1.t[1],b2=y1.t[2],c2=y1.t[3],d2=y1.t[4],e2=y1.t[5],
+                 a3=y2[1],b3=y2[2],c3=y2[3],d3=y2[4],e3=y2[5],
+                 a4=y22[1],b4=y22[2],c4=y22[3],d4=y22[4],e4=y22[5],f4=y22[6])
+  three <- tc.GENE(1,4,
+                   #a1=y1[1],b1=y1[2],c1=y1[3],d1=y1[4],e1=y1[5],
+                   #a2=0.2+y2[1],b2=0.2+y2[2],c2=0.2+y2[3],d2=0.2+y2[4],e2=0.2+y2[5],
+                   a3=y3[1],b3=y3[2],c3=y3[3],d3=y3[4],e3=y3[5],f3=y3[6],
+                   a4=y32[1],b4=y32[2],c4=y32[3],d4=y32[4],e4=y32[5],f4=y32[6])
+  #var41 = 0.2, var42 = 0.2, var43 = 0.2,var44 = 0.2,var45 = 0.2)
+  # four <- tc.GENE(1,4,a1=y1[1],b1=y1[2],c1=y1[3],d1=y1[4],e1=y1[5],
+  #                  a2=y1[1],b2=y1[2],c2=y1[3],d2=y1[4],e2=y1[5],
+  #                  a3=y2[1],b3=y2[2],c3=y2[3],d3=y2[4],e3=y2[5],
+  #                  a4=y2[1],b4=y2[2],c4=y2[3],d4=y2[4],e4=y2[5])
   flat <- tc.GENE(200,4)
-  df.final <- rbind(onediff,twodiff.2way,difftime,small,flat)
-  groups <- c(rep('patter in 2 and 3',10),rep('patter in 2 and 4',10),rep('patter in 2,3, and 4',10),rep('patter with single metabolite',1),rep('flat',nrow(flat)))
-  rownames(df.final) <- c(paste('met1-10',c(1:10)),paste('met11-20',c(1:10)),paste('met21-30',c(1:10)),'met31',paste('flat',1:nrow(flat)))
+  df.final <- rbind(one,two,three,flat)
+  groups <- c(rep('one',nrow(one)),rep('two',nrow(two)),rep('three',nrow(three))
+              ,rep('flat',nrow(flat)))
+  rownames(df.final) <- c(paste('one',1:nrow(one)),paste('two',1:nrow(two)),paste('three',1:nrow(three))
+                          ,paste('flat',1:nrow(flat)))
   output <- vector(mode='list',length = 2)
   names(output) <- c('df','groups')
   output[[1]] <- df.final
@@ -69,7 +76,7 @@ generate_data <- function(){
   output
 }
 
-generate_data.nocorrelation <- function(){
+generate_data.highcorrelation <- function(){
   #6 time points;2 treatments;4 replicates
   #no correlation between metabolites
   tc.GENE <- function(n, r,
@@ -103,7 +110,7 @@ generate_data.nocorrelation <- function(){
                      #a2=y1.t[1],b2=y1.t[2],c2=y1.t[3],d2=y1.t[4],e2=y1.t[5],
                      a3=y1[1],b3=y1[2],c3=y1[3],d3=y1[4],e3=y1[5],f3=y1[6],
                       a4=y12[1],b4=y12[2],c4=y12[3],d4=y12[4],e4=y12[5],f4=y12[6])
-  two <- tc.GENE(1,4,
+  two <- tc.GENE(10,4,
                           #a1=y1[1],b1=y1[2],c1=y1[3],d1=y1[4],e1=y1[5],
                           #a2=y1.t[1],b2=y1.t[2],c2=y1.t[3],d2=y1.t[4],e2=y1.t[5],
                           a3=y2[1],b3=y2[2],c3=y2[3],d3=y2[4],e3=y2[5],
@@ -120,9 +127,9 @@ generate_data.nocorrelation <- function(){
   #                  a4=y2[1],b4=y2[2],c4=y2[3],d4=y2[4],e4=y2[5])
   flat <- tc.GENE(200,4)
   df.final <- rbind(one,two,three,flat)
-  groups <- c(rep('one',nrow(one)),'two','three'
+  groups <- c(rep('one',nrow(one)),rep('two',nrow(two)),rep('three',nrow(three))
               ,rep('flat',nrow(flat)))
-  rownames(df.final) <- c(paste('one',1:nrow(one)),'two','three'
+  rownames(df.final) <- c(paste('one',1:nrow(one)),paste('two',1:nrow(two)),paste('three',1:nrow(three))
                           ,paste('flat',1:nrow(flat)))
   output <- vector(mode='list',length = 2)
   names(output) <- c('df','groups')
--- main_realdata.R
+++ main_realdata.R
@@ -2,15 +2,21 @@
 ########
 #real data
 ########
+setwd('../')
 library(readxl)
 colnames <- read_xlsx('macrophage%20polarization.xlsx',range = 'PeakArea!I1:BL1')
 df <- read_xlsx('macrophage%20polarization.xlsx',range = 'PeakArea!I4:BL43',col_names = colnames(colnames))
-rm(colnames)
-
+sums <- apply(df,1,sum, na.rm=T)
+df <- sweep(df*100,1,sums,"/")
+i=2
+j=5
+r=4
+time=c(0,2,6,10,24)
 ###exploration###
 plot(apply(log(df),2,mean),apply(log(df),2,sd))
-hist(df$met1)
-hist(log(df$met1))
+plot(apply(df,2,mean),apply(df,2,sd))
+hist(df$Adenosine[1:19])
+hist(log(df$Adenosine[1:19]))
 ###pretreatment####
 library(tidyverse)
 df$togroup <- rep(1:10,each=4)
@@ -18,19 +24,20 @@ df1 <- df %>%
   group_by(togroup) %>% 
   mutate_all(~ifelse(is.na(.x), mean(.x, na.rm = TRUE), .x))
 df1[1:4,"Fructose-1,6-bisphosphate"] <- df1$`Fructose-1,6-bisphosphate`[5]#fill NA with mean of replicates. F16B is missing values at entire time 1 control. filled with values at time 1 treatment
+df1[20,] <- df1[19,]
 df1$togroup <- NULL
-df1$Leucine <- NULL
-df.log <- log(df1)#log transform
-df.final <- t(scale(df.log))
+df1$Leucine <- NULL#leucine has lots of NA. so removed
+#df.log <- log(df1)#log transform
+df.final <- t(scale(df1))
 
 #####################masigpro#############
 
 ######
 #design matrix
 ######
-
+source('main.R')
 masigpro.design <- design_matrix(i,j,r,time)
-design <- make.design.matrix(edesign = masigpro.design,degree = 2)
+design <- make.design.matrix(edesign = masigpro.design,degree = 3)
 dis <- design$dis
 edesign <- design$edesign
 colnames(df.final) <- rownames(edesign)
@@ -39,13 +46,14 @@ colnames(df.final) <- rownames(edesign)
 #fit model
 ########
 masigpro.fit <- maSigPro(df.final,masigpro.design,degree=3)
-toplot <- rownames(masigpro.fit$sig.genes$treatment2vscontrol$sig.profiles)
+print(masigpro.fit$summary)
+toplot <- rownames(masigpro.fit$sig.genes$treatment2vstreatment1$sig.profiles)
 #######
 #plot
 ######
-see.genes(masigpro.fit$sig.genes$treatment2vscontrol,show.fit = T,dis = edesign,k = 3)
+see.genes(masigpro.fit$sig.genes$treatment2vstreatment1,show.fit = T,dis = edesign,k = 3)
 for (each in c(toplot)){
-  PlotGroups(df.final[rownames(df.final)==each,],edesign = edesign, show.fit = T, dis = dis, groups.vector = design$groups.vector)
+  PlotGroups(df.final[rownames(df.final)==each,],edesign = edesign, show.fit = T, dis = dis, groups.vector = design$groups.vector,main = each)
 }
 AA <- df.final["Ascorbic Acid",]
 PlotGroups(AA,edesign = edesign, show.fit = T, dis = dis, groups.vector = design$groups.vector)
@@ -54,26 +62,34 @@ PlotGroups(AA,edesign = edesign, show.fit = T, dis = dis, groups.vector = design
 ######
 #design matrix
 #####
-mx.i <- matrix(0,nrow = i*j*r, ncol = i, dimnames = list(c(),c('control','treatment')))+c(rep(1,j*r),rep(0,i*j*r))
-mx.j <- matrix(0,nrow = i*j*r, ncol = j, dimnames = list(c(),c('T0','T2','T6','T10','T24')))+c(rep(c(rep(1,r),rep(0,j*r-r)),i),rep(0,r))
-mx.ij <- matrix(0,nrow = i*j*r, ncol = i*j, dimnames = list(c(),c(paste('inter',1:(i*j)))))+c(rep(1,r),rep(0,i*j*r))
-
+# mx.i <- matrix(0,nrow = i*j*r, ncol = i, dimnames = list(c(),c('control','treatment')))+c(rep(1,j*r),rep(0,i*j*r))
+# mx.j <- matrix(0,nrow = i*j*r, ncol = j, dimnames = list(c(),c('T0','T2','T6','T10','T24')))+c(rep(c(rep(1,r),rep(0,j*r-r)),i),rep(0,r))
+# mx.ij <- matrix(0,nrow = i*j*r, ncol = i*j, dimnames = list(c(),c(paste('inter',1:(i*j)))))+c(rep(1,r),rep(0,i*j*r))
+mx <- asca.design.matrix(i,j,r,time)
 ######
 #fit model
 ######
-asca.fit <- ASCA.2f(X = t(df.final),Designa = mx.j, Designb = mx.i,type = 1, Fac = c(3,2,2,2))
+source('ASCA-genes.1.2.1/sourceASCA_mac.R')
+asca.fit <- ASCA.2f(X = t(df.final),Designa = mx$j, Designb = mx$i,type = 1, Fac = c(1,1,1,2))
+Fac=c(1,1,1,2)
+type=1
 ######
 #plot
 ######
-lev.lim <- leverage.lims(df.final,R=10,FUN = ASCA.2f,Designa = mx.j, Designb = mx.i,Fac = c(3,2,2,2),type = 1,alpha = 0.05)$Cutoff[[2]]
-spe.lim <- SPE.lims(my.asca = asca.fit,alpha = 0.05)[[2]]
-leverage <- asca.fit$Model.bab$leverage
-spe <- asca.fit$Model.bab$SPE
-lev.spe.toplot <- data.frame(leverage=leverage,spe=spe)
-lev.spe.toplot$metabolites <- rownames(df.final)
+#source('main.R')
+# # lev.lim <- leverage.lims(df.final,R=10,FUN = ASCA.2f,Designa = mx.j, Designb = mx.i,Fac = c(1,1,3,2),type = 1,alpha = 0.05)$Cutoff[[2]]
+# # spe.lim <- SPE.lims(my.asca = asca.fit,alpha = 0.05)[[2]]
+# # leverage <- asca.fit$Model.bab$leverage
+# # spe <- asca.fit$Model.bab$SPE
+# # lev.spe.toplot <- data.frame(leverage=leverage,spe=spe)
+# # lev.spe.toplot$metabolites <- rownames(df.final)
+# 
+# ggplot(data = lev.spe.toplot,aes(x=leverage,y=spe))+
+#   geom_point()+
+#   geom_hline(yintercept = spe.lim)+
+#   geom_vline(xintercept = lev.lim)+
+#   geom_text(aes(label=metabolites))
 
-ggplot(data = lev.spe.toplot,aes(x=leverage,y=spe))+
-  geom_point()+
-  geom_hline(yintercept = spe.lim)+
-  geom_vline(xintercept = lev.lim)+
-  geom_text(aes(label=metabolites))
+plot.leverage_spe(df.final,asca.fit,colnames)
+plot.submodels_score(asca.fit,i,j)
+plot.submodels_loading(asca.fit,colnames)
