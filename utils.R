2e5247f79153815a7e29b434a72432efdc9bbff0
gzu300 <zhuguanchen@me.com>
2019-02-25 01:24:23 +0100

    update rmd for report

--- utils.R
+++ utils.R
@@ -73,12 +73,11 @@ wrap.permutation.info <- function(df.final,asca.fit,groups,R,which_leverage,alph
   
   permutated.data <- leverage.lims(df.final,R=R,FUN = which_leverage,Designa = mx$j, Designb = mx$i,Fac = Fac,type = type,alpha = alpha,showvar = F, showscree = F)
   permu.data <- permutated.data$NullDistribution$Model.bab
-  lev.lim <- permutated.data$Cutoff[[2]]#leverage threshold
-  spe.lim <- SPE.lims(my.asca = asca.fit,alpha = 0.01)[[2]]#spe threshold
-  leverage <- asca.fit$Model.bab$leverage
-  spe <- asca.fit_leverage$Model.bab$SPE
-  selected_features <- sort(groups[leverage>lev.lim])#list of features' leverage > threshold
+  lev.lim <- permutated.data$Cutoff[[2]]
+  spe.lim <- SPE.lims(my.asca = asca.fit,alpha = alpha)[[2]]
   
+  leverage <- asca.fit$Model.bab$leverage
+  spe <- asca.fit$Model.bab$SPE
   #assemble dataframe for leverage vs spe as well as stats for prediction accuracy. FP, FN... can be calculated in stats function below
   lev.spe.toplot <- data.frame(leverage=leverage,
                                spe=spe,
@@ -87,17 +86,10 @@ wrap.permutation.info <- function(df.final,asca.fit,groups,R,which_leverage,alph
                                predicted=(leverage>lev.lim),
                                truth=(groups != 'flat')
                                )
-  
-  Nulldist_plot <- plot.NullDistribution(permu.data,asca.fit$Model.bab$leverage,groups,lev.lim,R)
-  
-  output <- list(lev_limit=lev.lim, 
-                 spe_lim=spe.lim, 
-                 stats_for_plot=lev.spe.toplot, 
-                 selected_features=selected_features, 
-                 Null_distribution_plot=Nulldist_plot)
-  
-  submodel_plots <- plot.submodels(output,asca.fit, groups=groups, Fac=Fac, size=10,...)
-  
+  selected_features <- sort(groups[leverage>lev.lim])
+  Nulldist_plot <- plot.NullDistribution(permu.data,asca.fit$Model.bab$leverage,groups,lev.lim,R,alpha)
+  output <- list(lev_limit=lev.lim, spe_lim=spe.lim, stats_for_plot=lev.spe.toplot, selected_features=selected_features, Null_distribution_plot=Nulldist_plot)
+  submodel_plots <- plot.submodels(output,asca.fit, groups=groups, Fac=Fac, size=6,...)
   output <- c(output, plots_for_submodel=submodel_plots)
   output
 }
@@ -115,6 +107,7 @@ stats <- function(permut_wrapped){
 
 #####plots########
 
+#plot.leverage_spe(df.final,asca.fit, groups)
 plot.leverage_spe <- function(permut_wrapped, sd=sd,title = paste('improved leverage and SPE with',Fac[3],'PCs'),size,...){
   plot <- ggplot(data = permut_wrapped$stats_for_plot,aes(x=leverage,y=spe,color=groups))+
     geom_point()+
@@ -191,17 +184,18 @@ plot_metabolites <- function(df,range,...){
     labs(...)
   output
 }
-plot_a_metabolite <- function(df,FUN,which,...){
+plot_a_metabolite <- function(df,FUN,which,size=6,...){
   df.final <- df
   trend.toplot <- data.frame(replicate=rep(1:(i*j),each=r),time=rep(time,each=r),treatment=factor(rep(1:i,each=j*r)),metabolite=df.final[which,])
   ggplot(trend.toplot,aes(x=time,y=metabolite,color=treatment))+
     geom_point()+
     stat_summary(fun.y = FUN,geom = 'line')+
     geom_smooth(method = 'lm', formula = y~poly(x,2),se = F,linetype = '3313')+
+    theme(axis.title = element_text(size=size),legend.title = element_text(size=size),legend.text = element_text(size=3),legend.key.size = unit(0.08,'cm'), plot.title = element_text(size=size))+
     labs(...)
 }
 
-plot.NullDistribution <- function(permu.data,model.data,colnames,cutoff,R){
+plot.NullDistribution <- function(permu.data,model.data,colnames,cutoff,R,alpha){
   permu.data <- permu.data
   model.data <- model.data
   model.leverage <- data.frame(metabolites=colnames[1:length(model.data)],leverage=model.data)
@@ -209,10 +203,10 @@ plot.NullDistribution <- function(permu.data,model.data,colnames,cutoff,R){
     gather(.,key = metabolites,value = leverage) %>% 
     mutate(.,metabolites=rep(colnames[1:nrow(permu.data)],each=ncol(permu.data))) %>% 
     ggplot(.,aes(factor(metabolites),leverage))+
-    geom_violin(draw_quantiles = c(0.95))+
+    geom_violin(draw_quantiles = c(1-alpha))+
     geom_point(data = model.leverage,aes(x = metabolites,y = leverage))+
     geom_hline(yintercept = cutoff)+
     theme(axis.text.x.bottom = element_text(angle = 90,size = 7,hjust = 1,vjust = 0.5))+
-    labs(title = paste('Null distribution by',R,'rounds of permutation'))
+    labs(title = paste('Null distribution by',R,'rounds of permutation. alpha:',alpha))
   Nulldist
 }
