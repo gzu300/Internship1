7dfb02fca0cacd339425edb73865c46074902803
gzu300 <zhuguanchen@me.com>
2019-02-28 17:41:14 +0100

    edit plots

--- utils.R
+++ utils.R
@@ -82,12 +82,12 @@ wrap.permutation.info <- function(df.final,asca.fit,groups,R,which_leverage,alph
   lev.spe.toplot <- data.frame(leverage=leverage,
                                spe=spe,
                                metabolites=1:nrow(df.final),
-                               groups=groups,
+                               patterns=groups,
                                predicted=(leverage>lev.lim),
                                truth=(groups != 'flat')
                                )
   selected_features <- sort(groups[leverage>lev.lim])
-  Nulldist_plot <- plot.NullDistribution(permu.data,asca.fit$Model.bab$leverage,groups,lev.lim,R,alpha)
+  Nulldist_plot <- plot.NullDistribution(permu.data,asca.fit$Model.bab$leverage,groups,lev.lim,R,alpha,size=6)
   output <- list(lev_limit=lev.lim, spe_lim=spe.lim, stats_for_plot=lev.spe.toplot, selected_features=selected_features, Null_distribution_plot=Nulldist_plot)
   submodel_plots <- plot.submodels(output,asca.fit, groups=groups, Fac=Fac, size=6,...)
   output <- c(output, plots_for_submodel=submodel_plots)
@@ -109,12 +109,12 @@ stats <- function(permut_wrapped){
 
 #plot.leverage_spe(df.final,asca.fit, groups)
 plot.leverage_spe <- function(permut_wrapped, sd=sd,title = paste('improved leverage and SPE with',Fac[3],'PCs'),size,...){
-  plot <- ggplot(data = permut_wrapped$stats_for_plot,aes(x=leverage,y=spe,color=groups))+
+  plot <- ggplot(data = permut_wrapped$stats_for_plot,aes(x=leverage,y=spe,color=patterns))+
     geom_point()+
     geom_hline(yintercept = permut_wrapped$spe_lim)+
     geom_vline(xintercept = permut_wrapped$lev_limit)+
     labs(title = title,...)+
-    theme(axis.title = element_text(size=size),legend.title = element_text(size=size),legend.text = element_text(size=3),legend.key.size = unit(0.08,'cm'), plot.title = element_text(size=size))
+    theme(axis.title = element_text(size=size),panel.background = element_blank(),legend.title = element_text(size=size),legend.text = element_text(size=3),legend.key.size = unit(0.08,'cm'), plot.title = element_text(size=size))
   plot
 }
 
@@ -154,7 +154,7 @@ plot.submodels_loading <- function(asca.fit,groups=NULL,title = paste('loading p
       geom_col(aes_string(y = colnames(bab.loadings)[each]))+
       ylab(paste('PC',each))+
       labs(title = title,...)+
-      theme(axis.title = element_text(size=size),legend.title = element_text(size=size),legend.text = element_text(size=3),legend.key.size = unit(0.08,'cm'), plot.title = element_text(size=size))
+      theme(axis.title = element_text(size=size),panel.background = element_blank(),legend.title = element_text(size=size),legend.text = element_text(size=3),legend.key.size = unit(0.08,'cm'), plot.title = element_text(size=size))
     output[[each]] <- plot
   }
   output
@@ -171,14 +171,14 @@ plot_metabolites <- function(df,range,...){
   df.final <- df
   df.toplot <- data.frame(t(df.final[range,]))
   df.toplot$time <- rep(rep(time,each=r),i)
-  df.toplot$treatment <- rep(1:2,each=r*j)
+  df.toplot$treatment <- factor(rep(1:2,each=r*j))
   a <- df.toplot %>% 
     gather(key = metabolites,value = value,1:length(range))
   
-  output <- ggplot(a,aes(x=time,y=value,color=factor(treatment)))+
+  output <- ggplot(a,aes(x=time,y=value,color=treatment))+
     geom_point(size=0.5)+
     stat_summary(fun.y = mean,geom = 'line')+
-    theme(legend.title = element_text(size = 5))+
+    theme(legend.title = element_text(size = 5),panel.background = element_blank())+
     facet_wrap(metabolites~., scales = 'free')+
     geom_smooth(method = 'lm', formula = y~poly(x,2),se = F,linetype = '3313')+
     labs(...)
@@ -191,22 +191,22 @@ plot_a_metabolite <- function(df,FUN,which,size=6,...){
     geom_point()+
     stat_summary(fun.y = FUN,geom = 'line')+
     geom_smooth(method = 'lm', formula = y~poly(x,2),se = F,linetype = '3313')+
-    theme(axis.title = element_text(size=size),legend.title = element_text(size=size),legend.text = element_text(size=3),legend.key.size = unit(0.08,'cm'), plot.title = element_text(size=size))+
+    theme(legend.position=c(0.9,0.1),axis.title = element_text(size=size),legend.title = element_text(size=size),legend.text = element_text(size=3),legend.key.size = unit(0.08,'cm'), plot.title = element_text(size=size),panel.background = element_blank())+
     labs(...)
 }
 
-plot.NullDistribution <- function(permu.data,model.data,colnames,cutoff,R,alpha){
+plot.NullDistribution <- function(permu.data,model.data,colnames,cutoff,R,alpha,size){
   permu.data <- permu.data
   model.data <- model.data
-  model.leverage <- data.frame(metabolites=colnames[1:length(model.data)],leverage=model.data)
+  model.leverage <- data.frame(metabolites=factor(colnames[1:length(model.data)]),leverage=model.data)
   Nulldist <- data.frame(permu.data) %>% 
     gather(.,key = metabolites,value = leverage) %>% 
     mutate(.,metabolites=rep(colnames[1:nrow(permu.data)],each=ncol(permu.data))) %>% 
-    ggplot(.,aes(factor(metabolites),leverage))+
+    ggplot(.,aes(metabolites,leverage,color=metabolites))+
     geom_violin(draw_quantiles = c(1-alpha))+
     geom_point(data = model.leverage,aes(x = metabolites,y = leverage))+
     geom_hline(yintercept = cutoff)+
-    theme(axis.text.x.bottom = element_text(angle = 90,size = 7,hjust = 1,vjust = 0.5))+
+    theme(axis.text.x.bottom = element_text(angle = 90,size = 7,hjust = 1,vjust = 0.5),panel.background = element_blank(),legend.position=c(0.5,0.8),axis.title = element_text(size=size),legend.title = element_text(size=size),legend.text = element_text(size=3),legend.key.size = unit(0.08,'cm'), plot.title = element_text(size=size))+
     labs(title = paste('Null distribution by',R,'rounds of permutation. alpha:',alpha))
   Nulldist
 }
